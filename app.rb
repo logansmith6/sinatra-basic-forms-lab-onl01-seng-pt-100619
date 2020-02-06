198b0da336d93d7388baf4b25d493e1d9f2410f2
Logan Smith <logsmith06@gmail.com>
2020-02-06 23:30:01 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -11,7 +11,7 @@ class App < Sinatra::Base
   end
 
   post '/puppy' do
-    pup = Puppy.new 
+    pup = Puppy.new
     pup(params[:name], params[:breed], params[:age])
     erb :display_puppy
   end
